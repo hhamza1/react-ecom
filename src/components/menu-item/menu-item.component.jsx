117f62c9e45263c392982340dea98a6cb3a1a2e6
unknown <hamzahmitou@gmail.com>
2020-03-30 13:03:07 +0300

    Menu-item styling and animation

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,16 +4,19 @@ import './menu-item.styles.scss';
 
 
 
-const MenuItem = ({title, imageUrl}) => {
+const MenuItem = ({title, imageUrl, size}) => {
     return (
-        <div style={{
-            backgroundImage : `url(${imageUrl})`
-        }} className="menu-item">
+    <div className={`menu-item ${size}`}>
+        <div 
+            style={{
+                backgroundImage : `url(${imageUrl})`
+            }} 
+            className='background-image' />
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">CLICK HERE</span>
             </div>
-        </div>
+    </div>
     )
 }
 
