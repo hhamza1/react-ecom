e6d5e67f517a7dc3cd2983f528f158c88e66f68f
Falcon2 <hamzahmitou@gmail.com>
2020-04-13 16:45:29 +0300

    Adding auth method to the component

--- src/App.js
+++ src/App.js
@@ -15,18 +15,25 @@ class App extends Component {
     }
   }
 
+  unsubscribeFromAuth = null;
+
   componentDidMount(){
-    auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({currentUser:user});
-      console.log(user);
     })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
 
 
   render() { 
+    const {currentUser} = this.state;
+
     return (
       <div>
-        <Header />
+        <Header currentUser={currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
