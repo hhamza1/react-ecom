0bbbe76466e6f7b77a9f259f44fd8ecb12b284e3
Falcon2 <hamzahmitou@gmail.com>
2020-04-13 16:46:57 +0300

    Passing authentication props to header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/logo.svg';
+import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = () => {
+const Header = ({currentUser}) => {
     return(
         <div className="header">
             <Link className="logo-container" to="/">
@@ -14,12 +15,18 @@ const Header = () => {
                 <Link className="option" to='/shop'>
                     SHOP
                 </Link>
-                <Link className="option" to='/signin'>
-                    SIGN IN
-                </Link>
                 <Link className="option" to='/shop'>
                     CONTACT
                 </Link>
+                {
+                    currentUser ? 
+                    <div>
+                        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                    </div> : 
+                    <Link className="option" to='/signin'>
+                        SIGN IN
+                    </Link>
+                }
             </div>
         </div>
     )
