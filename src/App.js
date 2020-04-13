eb9bd6048e82fb7123bb7b3a7096bfd0208a55ed
Falcon2 <hamzahmitou@gmail.com>
2020-04-13 15:38:05 +0300

    Switch to class component + save auth object

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInRegister from './pages/signin-register/signin-register.component';
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInRegister} />
-      </Switch>
-    </div>
-  );
-}
+import { auth } from './firebase/firebase.utils'
 
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser : null
+    }
+  }
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user => {
+      this.setState({currentUser:user});
+      console.log(user);
+    })
+  }
+
+
+  render() { 
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInRegister} />
+        </Switch>
+      </div>
+    );
+  }
+}
+ 
 export default App;
