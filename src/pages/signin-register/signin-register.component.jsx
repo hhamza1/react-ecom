c1e75eec9bc66259715ecc2dde2d9ec3d699bbcb
Falcon2 <hamzahmitou@gmail.com>
2020-04-15 21:16:34 +0300

    Added the sign up form the signin-register page

--- src/pages/signin-register/signin-register.component.jsx
+++ src/pages/signin-register/signin-register.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 import './signin-register.styles.scss';
 
 
@@ -7,6 +8,7 @@ const SignInRegister = () => {
     return(
         <div className="sign-in-register">
              <SignIn />
+             <SignUp />
         </div>
     )
 }
