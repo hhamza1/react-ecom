6e6bc3308882a46d3e303a955c21eb6161432634
Falcon2 <hamzahmitou@gmail.com>
2020-04-19 13:34:02 +0300

    Connecting cart item with its display component

--- src/components/cart-display/cart-display.component.jsx
+++ src/components/cart-display/cart-display.component.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import CartItem from '../cart-item/cart-item.component';
 import CustomBtn from '../custom-btn/custom-btn.component';
 import './cart-display.styles.scss';
 
-const CartDisplay = () => {
+const CartDisplay = ({cartItems}) => {
     return(
         <div className="cart-display">
-            <div className="cart-items"></div>
+            <div className="cart-items">
+                {cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)}
+            </div>
             <CustomBtn>go to checkout</CustomBtn>
         </div>
     );
 }
 
-export default CartDisplay;
+
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps, null)(CartDisplay);
