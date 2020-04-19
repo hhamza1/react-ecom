88fde630fe1abf3c6ecca8a96a71cac858173f9e
Falcon2 <hamzahmitou@gmail.com>
2020-04-19 19:33:51 +0300

    Hide cart when going to checkout page

--- src/components/cart-display/cart-display.component.jsx
+++ src/components/cart-display/cart-display.component.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {withRouter} from 'react-router-dom';
 import CartItem from '../cart-item/cart-item.component';
 import CustomBtn from '../custom-btn/custom-btn.component';
 import './cart-display.styles.scss';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import  {toggleHiddenCart} from '../../redux/cart/cart.actions';
 
-const CartDisplay = ({cartItems}) => {
+const CartDisplay = ({cartItems, history, dispatch}) => {
     return(
         <div className="cart-display">
             <div className="cart-items">
-                {cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)}
+                {
+                    cartItems.length 
+                    ? (cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />))
+                    : (<span class="empty-cart">The cart is empty</span>)
+                }
             </div>
-            <CustomBtn>go to checkout</CustomBtn>
+            <CustomBtn onClick={() => 
+                {
+                    history.push("/checkout");
+                    dispatch(toggleHiddenCart());
+                }}>go to checkout
+            </CustomBtn>
         </div>
     );
 }
@@ -22,4 +33,4 @@ const mapStateToProps = (state) => ({
     cartItems : selectCartItems(state)
 });
 
-export default connect(mapStateToProps, null)(CartDisplay);
+export default withRouter(connect(mapStateToProps)(CartDisplay));
