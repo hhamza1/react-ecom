8c4825569760754a2d8a3ce61e4f73040f1b539e
Falcon2 <hamzahmitou@gmail.com>
2020-04-19 12:11:18 +0300

    Added the add item to cart action

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,4 +4,9 @@ export const toggleHiddenCart = () => ({
     type: CartActionTypes.TOGGLE_HIDDEN_CART
 });
 
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
+
 
