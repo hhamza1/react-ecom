583e40138e02709ffee3dcdba9b0d8adbbf6f592
Falcon2 <hamzahmitou@gmail.com>
2020-04-19 19:36:16 +0300

    Added new selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -3,6 +3,23 @@ import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
-export const selectCartItems = createSelector([selectCart],(cart) => cart.cartItems);
+export const selectHidden = createSelector(
+    [selectCart], (cart) => cart.hidden
+);
 
-export const selectCartItemsCount = createSelector([selectCartItems], (cartItems) => cartItems.reduce((acc, cartItem) => acc + cartItem.qty, 0))
+export const selectCartItems = createSelector(
+    [selectCart],(cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems], 
+    (cartItems) => cartItems.reduce(
+        (acc, cartItem) => acc + cartItem.qty, 0)
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    (cartItems) => cartItems.reduce(
+        (acc, cartItem) => acc + cartItem.price * cartItem.qty
+        ,0) 
+)
