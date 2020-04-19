e895b374914e5fcb816a3c15ab6448df1c182a72
Falcon2 <hamzahmitou@gmail.com>
2020-04-19 19:33:19 +0300

    Added the user selector to mapStateToProps

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { connect } from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Checkout from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import SignInRegister from './pages/signin-register/signin-register.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions';
 
+
+import {selectCurrentUser} from './redux/user/user.selectors';
 
 class App extends Component {
 
@@ -51,6 +54,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={Checkout} />
           <Route 
             exact 
             path="/signin" 
@@ -69,7 +73,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  currentUser : state.user.currentUser
+  currentUser : selectCurrentUser(state)
 });
  
 const mapDispatchToProps = dispatch => ({
