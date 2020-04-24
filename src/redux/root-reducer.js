41a4a7acbe98add906b5c71fb3cb6bf6005b378f
Falcon2 <hamzahmitou@gmail.com>
2020-04-24 23:15:02 +0300

    Added redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,5 +1,8 @@
 import {combineReducers} from 'redux';
 
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
