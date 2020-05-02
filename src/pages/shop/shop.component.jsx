2909996daf51d2b8698e5b1cff61ad6332f9af7f
Falcon2 <hamzahmitou@gmail.com>
2020-05-02 09:25:03 +0300

    Segregated the collection from shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import PreviewCollection from '../../components/preview-collection/preview-collection.component';
-import {connect} from 'react-redux';
 
-import {createStructuredSelector} from 'reselect';
+import {Route} from 'react-router-dom';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = ({ collections }) => {
+import CollectionPage from '../collection/collection.component';
+
+
+const ShopPage = ({ match }) => {
     return(
         <div className="shop-page">
-            {
-                collections.map(({id, ...collectionProps}) => (
-                    <PreviewCollection key={id} {...collectionProps} />
-                ))
-            }
+            <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
         </div>
     )
 }
 
-const mapStateToProps = createStructuredSelector({
-        collections: selectCollections
-});
 
-export default connect(mapStateToProps, null)(ShopPage);
+export default ShopPage;
