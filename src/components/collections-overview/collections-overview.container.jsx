cedc7b095055ca4d9f0c2b4cb60b599a137c54e2
Falcon2 <hamzahmitou@gmail.com>
2020-05-11 00:15:09 +0300

    Set containers for collectionPage & collectionOverview

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {compose} from 'redux';
+
+import  {createStructuredSelector} from 'reselect';
+import {selectIsCollectionFetching} from '../../redux/shop/shop.selectors';
+
+import LoadSpinner from '../load-spinner/load-spinner.component';
+import CollectionsOverview from './collections-overview.component';
+
+
+
+
+const mapStateToProps = createStructuredSelector({
+    isLoading : selectIsCollectionFetching
+});
+
+const CollectionsOverviewContainer = compose(
+    connect(mapStateToProps, null),
+    LoadSpinner
+)(CollectionsOverview)
+
+
+export default CollectionsOverviewContainer;
+
